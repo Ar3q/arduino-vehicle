3d59d555243bd28c948e10fcb43ee02139fb44fc
Ar3q <arek0906@protonmail.com>
2019-07-11 12:06:53 +0200

    Added ability to move after Arduino receives correct information

--- ArduinoVehicle.ino
+++ ArduinoVehicle.ino
@@ -1,6 +1,6 @@
 #include <SoftwareSerial.h>
 
-SoftwareSerial BTserial(14,15); //RX - A0, TX - A1
+SoftwareSerial BTserial(14, 15); //RX - A0, TX - A1
 
 //A right
 int n1 = 2; //dir
@@ -28,97 +28,131 @@ void setup() {
   pinMode(n3, OUTPUT);
   pinMode(n4, OUTPUT);
   pinMode(enb, OUTPUT);
-
-  /*
-  backwardRight(255);
-  backwardLeft(255);
-  delay(5000);
-  brakeRight(255);
-  brakeLeft(255);
-
-  delay(2000);
-  backwardRight(255);
-  backwardLeft(255);
-  delay(5000);
-  brakeRight(0);
-  brakeLeft(0);
-
-  delay(2000);
-  turnLeftBackward(255);
-  delay(2000);
-  brakeAll(255);
-  */
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if (BTserial.available() > 0){
+  if (BTserial.available() > 0) {
     byte receivedChar = BTserial.read();
-    if (receivedChar == lastCharFromBT){
+    if (receivedChar == lastCharFromBT) {
       return;
     }
     lastCharFromBT = receivedChar;
-    Serial.println(lastCharFromBT); 
+    Serial.println((char)lastCharFromBT);
+
+    move();
   }
 }
 
-void forwardRight(byte speedPWM){
+void move() {
+  switch ((char)lastCharFromBT) {
+    case '1':
+      Serial.println("Forward");
+      forwardAll(255);
+      break;
+
+    case '2':
+      Serial.println("Backward");
+      backwardAll(255);
+      break;
+
+    case '3':
+      Serial.println("Forward Left");
+      forwardLeft(255);
+      break;
+
+    case '4':
+      Serial.println("Forward Right");
+      forwardRight(255);
+      break;
+
+    case '5':
+      Serial.println("Backward Left");
+      backwardLeft(255);
+      break;
+
+    case '6':
+      Serial.println("Backward Right");
+      backwardRight(255);
+      break;
+
+    case '9':
+      Serial.println("Brake");
+      brakeAll(255);
+      break;
+
+    default:
+      Serial.println("??????");
+  }
+}
+
+void forwardRight(byte speedPWM) {
   analogWrite(ena, speedPWM);
   digitalWrite(n1, HIGH);
   digitalWrite(n2, LOW);
 }
 
-void forwardLeft(byte speedPWM){
+void forwardLeft(byte speedPWM) {
   analogWrite(enb, speedPWM);
   digitalWrite(n3, LOW);
   digitalWrite(n4, HIGH);
 }
 
-void backwardRight(byte speedPWM){
+void forwardAll(byte speedPWM) {
+  forwardRight(speedPWM);
+  forwardLeft(speedPWM);
+}
+
+void backwardRight(byte speedPWM) {
   analogWrite(ena, speedPWM);
   digitalWrite(n1, LOW);
   digitalWrite(n2, HIGH);
 }
 
-void backwardLeft(byte speedPWM){
+void backwardLeft(byte speedPWM) {
   analogWrite(enb, speedPWM);
   digitalWrite(n3, HIGH);
   digitalWrite(n4, LOW);
 }
 
-void brakeRight(byte speedPWM){
-  analogWrite(ena, speedPWM);
-  digitalWrite(n1, LOW);
-  digitalWrite(n2, LOW); 
+void backwardAll(byte speedPWM) {
+  backwardRight(speedPWM);
+  backwardLeft(speedPWM);
 }
 
-void brakeLeft(byte speedPWM){
+void brakeRight(byte speedPWM) {
+  analogWrite(ena, speedPWM);
+  digitalWrite(n1, LOW);
+  digitalWrite(n2, LOW);
+}
+
+void brakeLeft(byte speedPWM) {
   analogWrite(enb, speedPWM);
   digitalWrite(n3, LOW);
   digitalWrite(n4, LOW);
 }
 
-void brakeAll(byte speedPWM){
+void brakeAll(byte speedPWM) {
   brakeRight(speedPWM);
   brakeLeft(speedPWM);
 }
 
-void turnRightForward(byte speedPWM){
+void turnRightForward(byte speedPWM) {
   brakeRight(255);
   forwardLeft(speedPWM);
 }
 
-void turnRightBackward(byte speedPWM){
+void turnRightBackward(byte speedPWM) {
   brakeRight(255);
   backwardLeft(speedPWM);
 }
 
-void turnLeftForward(byte speedPWM){
+void turnLeftForward(byte speedPWM) {
   brakeLeft(255);
   forwardRight(speedPWM);
 }
 
-void turnLeftBackward(byte speedPWM){
+void turnLeftBackward(byte speedPWM) {
   brakeLeft(255);
   backwardRight(speedPWM);
 }
